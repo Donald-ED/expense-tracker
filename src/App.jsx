69a722691967e889417fe956d46028087854d20c
Donald Echefu <donaldechefu@Donalds-MacBook-Pro.local>
2024-08-29 15:32:45 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { generateClient } from "aws-amplify/data";
-import outputs from "../../amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
